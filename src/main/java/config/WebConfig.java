ec0def769ad7077561a875ca0807d6e0cee0cf60
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-06-12 21:55:39 +0700

    a

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,47 @@
+package config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("controller") //Scan den thu muc con troller
+public class WebConfig implements WebMvcConfigurer , ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    InternalResourceViewResolver resolver() {
+        System.out.println("GOI WebConfig");
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/view"); //tuong ứng config xuống webapp/view
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+//    @Bean
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(templateResolver());
+//        return templateEngine;
+//    }
+}
